e319673c123c4e9b746aab7fea3cfdd48ca08c6b
ASnugglyBear <Asnugglybearonreddit@gmail.com>
2017-08-04 14:29:39 +0000

    Updates directory to paralell bg3po

--- r2_supervisor.sh
+++ r2_supervisor.sh
@@ -5,11 +5,12 @@ for pid in $(pidof -x r2_supervisor.sh); do
       echo "Supervisor is still running, stopping myself (I am a replacement supervisor)"
       exit 0
     fi
-done 
+done
+
 
 function run_robot {
     echo "Starting r2d8"
-    /usr/bin/python2 /home/r2d8/r2d8_scripts/artoodeeeight.py 2>&1 /home/r2d8/logs.r2d8
+    /usr/bin/python2 /home/r2d8/r2d8-scripts/artoodeeeight.py >> /home/r2d8/r2d8-scripts/logs/r2d8.log 2>&1
 }
 
 
