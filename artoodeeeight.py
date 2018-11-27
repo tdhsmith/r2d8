dde5d7187f660a51313b1c06adb0e6d110563549
Taylor DH Smith <tdsmith@safetyweb.org>
2018-11-27 14:20:01 -0600

    Add more config options

--- artoodeeeight.py
+++ artoodeeeight.py
@@ -12,17 +12,44 @@ from r2d8_oauth import login as oauth_login
 
 log = logging.getLogger(__name__)
 
-def run_bot():
+def start_bot():
     ap = argparse.ArgumentParser()
     botname = 'r2d8'
     dbname = '{}-bot.db'.format(botname)
     sleepTime = 5
-    ap.add_argument(u'-d', u'--database', help=u'The bot database. Default is {}'.format(dbname),
-                    default=u'{}'.format(dbname))
-    ap.add_argument('-r', '--read', help='Mark all existing queries as read without responding, then exit.',
-                    default=False, action='store_true', dest="mark_read")
-    ap.add_argument('-s', '--sleep', help=u'Time to sleep between API checks. Default is {} seconds.'.format(sleepTime),
-                    default=sleepTime)
+
+    ap.add_argument(
+        u'-d',
+        u'--database',
+        help=u'The bot database. Default is {}'.format(dbname),
+        default=u'{}'.format(dbname))
+    ap.add_argument(
+        '-r',
+        '--read',
+        help='Mark all existing queries as read without responding, then exit.',
+        action='store_true', # default is false, flag present => true
+        dest='mark_read')
+    ap.add_argument(
+        '-s',
+        '--sleep',
+        help=u'Time to sleep between API checks. Default is {} seconds.'.format(sleepTime),
+        default=sleepTime,
+        type=int)
+    ap.add_argument(
+        '-o',
+        '--once',
+        help=u'Run once and quit', 
+        action='store_true')
+    ap.add_argument(
+        '-t',
+        '--target',
+        help=u'Run the tool on a specific comment ID target',
+        default=None)
+    ap.add_argument(
+        '-c',
+        '--config',
+        help=u'Config file location for PRAW',
+        default=None)
     addLoggingArgs(ap)
     args = ap.parse_args()
     handleLoggingArgs(args)
@@ -37,22 +64,41 @@ def run_bot():
     logging.getLogger(u"prawcore").setLevel(logging.WARNING)
     logging.getLogger(u"urllib3").setLevel(logging.WARNING)
 
-    reddit = oauth_login()
+    reddit = oauth_login(config_file_path = args.config) if args.config else oauth_login()
 
     bdb = BotDatabase(args.database)
     log.info(u'Bot database opened/created.')
     ch = CommentHandler(botname, bdb)
     log.info(u'Comment/notification handler created.')
-    botcmds = re.compile(u'/?u/{}\s(\w+)'.format(botname), re.IGNORECASE)
-    cmdmap = {
+
+    CMDMAP = {
         u'getinfo': ch.getInfo,
         u'repair': ch.repairComment,
         u'xyzzy': ch.xyzzy,
         u'alias': ch.alias,
         u'getaliases': ch.getaliases,
         u'getparentinfo': ch.getParentInfo,
-        u'getinfoparent': ch.getParentInfo
+        u'getinfoparent': ch.getParentInfo,
+        u'getthreadinfo': ch.getThreadInfo
     }
+    BOTCMD_REGEX = re.compile(u'/?u/{}\s(\w+)'.format(botname), re.IGNORECASE)
+
+    # target is like once, but we don't even need to scan for mentions
+    if args.target:
+        log.info(u'Executing specific target')
+        comment = reddit.comment(args.target)
+        if not bdb.comment_exists(comment):
+            bdb.add_comment(comment)
+            if args.mark_read:
+                return
+        for cmd in [c.lower() for c in BOTCMD_REGEX.findall(comment.body)]:
+            if cmd in CMDMAP:
+                comment.body = hp.unescape(comment.body)
+                CMDMAP[cmd](comment)
+            else:
+                log.info(u'Got unknown command: {}'.format(cmd))
+        return
+
     log.info(u'Waiting for new PMs and/or notifications.')
     while True:
         try:
@@ -63,10 +109,10 @@ def run_bot():
                     if args.mark_read:
                         continue
 
-                    for cmd in [c.lower() for c in botcmds.findall(comment.body)]:
-                        if cmd in cmdmap:
+                    for cmd in [c.lower() for c in BOTCMD_REGEX.findall(comment.body)]:
+                        if cmd in CMDMAP:
                             comment.body = hp.unescape(comment.body)
-                            cmdmap[cmd](comment)
+                            CMDMAP[cmd](comment)
                         else:
                             log.info(u'Got unknown command: {}'.format(cmd))
 
@@ -74,10 +120,10 @@ def run_bot():
             log.error(u'Caught exception: {}'.format(e))
 
         # get_mentions is non-blocking
-        if args.mark_read:
+        if args.mark_read or args.once:
             exit(0)
 
         sleep(sleepTime)
 
 if "__main__" == __name__:
-    run_bot()
+    start_bot()
--- r2d8_oauth.py
+++ r2d8_oauth.py
@@ -3,18 +3,18 @@ import ConfigParser
 
 log = logging.getLogger(__name__)
 
-config_file_name = "/home/r2d8/.config/praw.ini"
+default_config_location = "/home/r2d8/.config/praw.ini"
 from pathlib import Path
 
-config_file = Path(config_file_name)
+config_file = Path(default_config_location)
 if not config_file.is_file():
-    print("Missing praw.ini value with /r/boardgames customized values, stick it in ", config_file_name)
+    print("Missing praw.ini value with /r/boardgames customized values, stick it in ", default_config_location)
   
-def login():
+def login(config_file_path = default_config_location):
     import praw
     #I'm parsing the config file because praw isn't....  
     config = ConfigParser.ConfigParser()
-    config.read(config_file_name)
+    config.read(config_file_path)
     # todo: make it error out on file missing
     print("found: ", config.sections(), " if this doens't have r2d8_helper_scripts, you have a generic file")
     section_name = 'r2d8_helper_scripts'
